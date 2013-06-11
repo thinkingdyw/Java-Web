7672be9dde2d4f6795e6f5aaa021913f4ab9457c
thinkingdyw <thinkingdyw@gmail.com>
2013-06-11 14:09:18 +0800

    change utils

--- commons/src/main/java/com/company/common/utils/web/RequestUtils.java
+++ commons/src/main/java/com/company/common/utils/web/RequestUtils.java
@@ -18,73 +18,86 @@ import com.company.common.utils.web.convertor.DateConvertor;
 
 public final class RequestUtils {
 
-	
-	public static <T> T toBean(Class<T> clazz,HttpServletRequest request) throws Exception{
+	public static <T> T toBean(Class<T> clazz, HttpServletRequest request)
+			throws Exception {
 		T obj = instantiate(clazz);
 		initDefaultConvertor();
 		BeanUtils.populate(obj, request.getParameterMap());
 		return obj;
 	}
-	public static <T> T toBean(Class<T> clazz,HttpServletRequest request,Map<Class<?>,Converter> convertors) throws Exception{
+
+	public static <T> T toBean(Class<T> clazz, HttpServletRequest request,
+			Map<Class<?>, Converter> convertors) throws Exception {
 		T obj = instantiate(clazz);
 		initConvertor(convertors);
 		BeanUtils.populate(obj, request.getParameterMap());
 		return obj;
 	}
-	public static <T> T toBean(Class<T> clazz,HttpServletRequest request,String datePattern) throws Exception{
+
+	public static <T> T toBean(Class<T> clazz, HttpServletRequest request,
+			String datePattern) throws Exception {
 		T obj = instantiate(clazz);
 		initDefaultConvertor();
 		BeanUtils.populate(obj, request.getParameterMap());
 		return obj;
 	}
-	public static <T> T toBean(Class<T> clazz,Map<String, Object> params) throws Exception{
+
+	public static <T> T toBean(Class<T> clazz, Map<String, Object> params)
+			throws Exception {
 		T obj = instantiate(clazz);
 		initDefaultConvertor();
 		BeanUtils.populate(obj, params);
 		return obj;
 	}
-	public static <T> T toBean(Class<T> clazz,Map<String, Object> params,Map<Class<?>,Converter> convertors) throws Exception{
+
+	public static <T> T toBean(Class<T> clazz, Map<String, Object> params,
+			Map<Class<?>, Converter> convertors) throws Exception {
 		T obj = instantiate(clazz);
 		initConvertor(convertors);
 		BeanUtils.populate(obj, params);
 		return obj;
 	}
-	public static <T> T toBean(Class<T> clazz,Map<String, Object> params,String datePattern) throws Exception{
+
+	public static <T> T toBean(Class<T> clazz, Map<String, Object> params,
+			String datePattern) throws Exception {
 		T obj = instantiate(clazz);
 		initDefaultConvertor();
 		BeanUtils.populate(obj, params);
 		return obj;
 	}
+
 	/**
 	 * 自定义默认类型转换器,如果类型为引用类型，若果转换失败，则设置默认值为null
 	 */
-	private static void initDefaultConvertor(){ 
+	private static void initDefaultConvertor() {
 		ConvertUtils.register(new DateConvertor(), Date.class);
-		ConvertUtils.register(new LongConverter(null), Long.class);  
-	    	ConvertUtils.register(new ShortConverter(null), Short.class);  
-	    	ConvertUtils.register(new IntegerConverter(null), Integer.class);  
-	    	ConvertUtils.register(new DoubleConverter(null), Double.class);  
-	    	ConvertUtils.register(new BigDecimalConverter(null), BigDecimal.class); 
+		ConvertUtils.register(new LongConverter(null), Long.class);
+		ConvertUtils.register(new ShortConverter(null), Short.class);
+		ConvertUtils.register(new IntegerConverter(null), Integer.class);
+		ConvertUtils.register(new DoubleConverter(null), Double.class);
+		ConvertUtils.register(new BigDecimalConverter(null), BigDecimal.class);
 	}
+
 	/**
 	 * 初始化转换器
+	 * 
 	 * @param convertors
 	 */
-	private static void initConvertor(Map<Class<?>,Converter> convertors){
+	private static void initConvertor(Map<Class<?>, Converter> convertors) {
 		initDefaultConvertor();
 		for (Map.Entry<Class<?>, Converter> convertor : convertors.entrySet()) {
 			ConvertUtils.register(convertor.getValue(), convertor.getKey());
 		}
-		
+
 	}
+
 	public static <T> T instantiate(Class<T> clazz) throws Exception {
 		if (clazz.isInterface()) {
 			throw new Exception("Specified class is an interface");
 		}
 		try {
 			return clazz.newInstance();
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			throw new Exception("Is it an abstract class?", ex);
 		}
 	}
