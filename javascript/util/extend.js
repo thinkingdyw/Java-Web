c77fe14e873440edd46e9b7c86debf85e2ae592e
thinkingdyw <thinkingdyw@gmail.com>
2015-01-03 20:35:51 +0800

    Update extend.js

--- javascript/util/extend.js
+++ javascript/util/extend.js
@@ -1,4 +1,36 @@
-$(function(window,undefined){
+/**
+ * 案例
+ * function Person(age){
+ *	this.age = age;
+ * }
+ * Person.prototype.getAge = function(){
+ *	return this.age;
+ * }
+ * function Baby(name,age){
+ *	Baby.super.constructor.call(this,age);
+ *	this.name = name;
+ *	this.getAge = function(){
+ *		return Baby.super.getAge.call(this);
+ *	}
+ * }
+ * 
+ * Baby.extend(Person);
+ * var baby = new Baby('xiaodiao',1);
+ * alert(baby.getAge());
+ * 
+ * var person = clone({
+ * 	name:'',
+ * 	age:1,
+ * 	getName:function(){
+ *		return this.name;
+ *	},
+ * 	getAge:function(){
+ * 		return this.age;
+ *	}
+ * });
+ * alert(person.getAge());
+ **/
+(function(window,undefined){
 	Function.prototype.extend = function(superClass){
 		var F = function(){};
 		F.prototype = superClass.prototype;
@@ -6,5 +38,13 @@ $(function(window,undefined){
 		this.prototype.constructor = this;
 
 		this.super = superClass.prototype;
+		if(superClass.prototype.constructor == Object.prototype.constructor){
+			superClass.prototype.constructor = superClass;
+		}
+	}
+	window.clone = function(obj){
+		var F = function(){}
+		F.prototype = obj;
+		return  new F();
 	}
 })(window);
